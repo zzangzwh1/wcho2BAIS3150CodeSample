c6cd3062b60ec59d07b8f37f6a5baf3c301a3320
Wonhyuk Cho <wcho2@nait.ca>
2023-11-08 13:30:38 -0700

    Completed!

--- wcho2BAIS3150CodeSample/Pages/FindProgram.cshtml.cs
+++ wcho2BAIS3150CodeSample/Pages/FindProgram.cshtml.cs
@@ -7,41 +7,39 @@ namespace wcho2BAIS3150CodeSample.Pages
 {
     public class FindProgramModel : PageModel
     {
+        public Models.Program ActivePrgoram = null;
         public List<string> ProgramCode = new List<string>();
         public string Message { get; set; } = string.Empty;
 
         [BindProperty]
         public string SelectValue { get; set; } = string.Empty;
 
-        //public Models.Program ActivePrgoram = null;
         public void OnGet()
         {
-            BCS requestDirector = new BCS();
-            Models.Program ActivePrgoram = requestDirector.FindProgram("BAIST");
-            string s = "";
+  /*         BCS requestDirector = new BCS();
+            ActivePrgoram = requestDirector.FindProgram("BAIST");
+            string s = "";*/
             GetProgramCode();
             Message = "Please Select Exists Program Code";
         }
         public void OnPost()
         {
+            
+
          
             if (string.IsNullOrEmpty(SelectValue))
             {
-                ModelState.AddModelError("SelectValue", "selectValue Must be Selected!");
+                ModelState.AddModelError("SelectValue", "SelectValue Must be Selected!");
 
             }
-            BCS requestDirector = new BCS();
-            Models.Program ActivePrgoram = requestDirector.FindProgram(SelectValue);
-            string s = "";
-            foreach (var a in ActivePrgoram.EnrolledStudents)
-            {
-                Console.WriteLine(a);
-            }
+          
 
             if (ModelState.IsValid )
             {
-             
-           
+                string s = "";
+                BCS requestDirector = new BCS();
+                ActivePrgoram = requestDirector.FindProgram(SelectValue);
+
                 GetProgramCode();
                 
 
